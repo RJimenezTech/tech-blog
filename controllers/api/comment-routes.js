fd22d0dd1f501424fcbb82e8d949d491b62d6800
Robert Jimenez <robertjimenez86@gmail.com>
2022-06-04 10:56:16 -0500

    add and tested ability to comment

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -12,11 +12,10 @@ router.get("/", (req, res) => {
 });
 
 router.post("/", withAuth, (req, res) => {
-  // expects => {comment_text: "This is the comment", user_id: 1, blog_id: 2}
   Comment.create({
     comment_text: req.body.comment_text,
     user_id: req.session.user_id,
-    blog_id: req.session.blog_id,
+    blog_id: req.body.blog_id,
   })
     .then((dbCommentData) => res.json(dbCommentData))
     .catch((err) => {
